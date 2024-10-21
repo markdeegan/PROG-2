4da8b5c28b49de5a39ae5c629141a8c8e9797494
Mark Deegan <1252943+markdeegan@users.noreply.github.com>
2024-10-21 16:41:40 +0100

    Create 01Hello.c programme
    Creating basic Hello Mark C programme

---
+++ src/01Hello.c
@@ -0,0 +1,23 @@
+/*
+Mark Deegan Mon 21 Oct 2024 16:33:33 IST
+Simple programme to print Hello World
+This is used for demonstrating the basics of C programming
+and how to compile and run such programmes
+MD20241021-01
+*/
+
+/* this is a pre-processor directive and it tells the compiler to 
+include the definitions contained in stdio.h when compiling this code a
+That file defines, among other things, the printf() function that we 
+use below */
+#include <stdio.h>
+
+
+/* start defining the main() method 
+Every C programme requires one and only one main method which is where the
+programme will commence execution */
+
+int main()
+{ 				/* start of main method */
+	printf("Hello Mark\n");	/* print a basic message */
+}				/* end of main method */
